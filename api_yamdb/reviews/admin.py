631086c72a1b90ba2170c5e1e2966308e88458c0
Roman Zemliakov <zemliakovroman97@gmail.com>
2024-03-21 17:56:27 +0100

    version0

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
+
+User = get_user_model()
+
+admin.site.empty_value_display = 'Не задано'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Настройка административного интерфейса для пользователей."""
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'bio', 'role')
+    list_editable = ('role',)
+    search_fields = ('role', 'username')
+    list_filter = ('role', 'username')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Настройки административного интерфейса для категорий."""
+    list_display = ('pk', 'name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Настройки административного интерфейса для жанров."""
+    list_display = ('pk', 'name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    """Настройки административного интерфейса для произведений."""
+    list_display = ('pk', 'name', 'year', 'description', 'category')
+    list_filter = ('name',)
+    search_fields = ('name', 'year', 'category')
+
+
+@admin.register(GenreTitle)
+class GenreTitleAdmin(admin.ModelAdmin):
+    """Настройки административного интерфейса
+    для соответствия жанров и произведений.
+    """
+    list_display = ('pk', 'genre', 'title')
+    list_filter = ('genre',)
+    search_fields = ('title',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Настройки административного интерфейса для отзывов."""
+    list_display = ('pk', 'title', 'text', 'author', 'score', 'pub_date')
+    search_fields = ('title', 'text')
+    list_filter = ('pub_date', 'author')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Настройки административного интерфейса для комментариев."""
+    list_display = ('pk', 'review', 'text', 'author', 'pub_date')
+    search_fields = ('review', 'text')
+    list_filter = ('pub_date', 'author')
--- api_yamdb/reviews/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils import timezone
-
-
-def year_validator(year):
-    if year > timezone.now().year:
-        raise ValidationError(
-            'Год не может быть больше текущего года.'
-        )
